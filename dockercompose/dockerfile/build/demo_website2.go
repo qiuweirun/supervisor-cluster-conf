3b4f3bd52d07351db0bcb4bbf67e558c66492100
unknown <1023102176@qq.com>
2021-12-02 20:21:09 +0800

    demo 站点源码

---
+++ dockercompose/dockerfile/build/demo_website2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func GetIP() (string) {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("net.Interfaces failed, err:", err.Error())
+	}
+
+	var ip string
+	Loop:
+	for i := 0; i < len(netInterfaces); i++ {
+		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
+			addrs, _ := netInterfaces[i].Addrs()
+
+			for _, address := range addrs {
+				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil {
+						ip = ipnet.IP.String()
+						break Loop
+					}
+				}
+			}
+		}
+	}
+	return ip
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	name, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Fprintln(w, GetIP()+" demo.website2.com")
+	fmt.Fprintln(w, "\r\n")
+	fmt.Fprintln(w, "主机名："+name)
+	fmt.Fprintln(w, "\r\n")
+	fmt.Fprintln(w, "进程ID:"+strconv.Itoa(os.Getpid()));
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	http.ListenAndServe(":8088", nil)
+}
